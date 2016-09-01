c9525fe7c02d7ed2ba89542ba5770dbdcaad427f
Paul Herz <phrz@me.com>
2016-09-01 12:53:54 -0500

    Done with all of page one instructions except error calc

--- src/proj1_a.cpp
+++ src/proj1_a.cpp
@@ -46,12 +46,49 @@ int main(int argc, const char * argv[]) {
 	
 	// Compute the vector p4 as the value of the
 	// Taylor polynomial p4(x) for all points x ∈ z.
-	Matrix c4 = eulerExponentialTaylorCoefficients(4);
+	// (ibid. for p8, p12)
 	
-	for(double x: z.row(0)) {
-		double p = nest(c4, x);
-		std::cout << "e^x, series = 4, x = " << x << ", p = " << p << std::endl;
+	// calculate 12 coefficients for p12 and truncate for the
+	// p8 and p4 calculation.
+	Matrix c12 = eulerExponentialTaylorCoefficients(12);
+	Matrix c8 = c12.range(0, 0, 0, 7);
+	Matrix c4 = c12.range(0, 0, 0, 3);
+	
+	// create the result vectors, each of which will hold
+	// p?(x) for all x in z.
+	Vector p4(z.size()),
+		   p8(z.size()),
+		   p12(z.size());
+	
+	// group up the coefficient row matrices with the
+	// corresponding result storage vectors
+	std::pair<Matrix*,Vector*> pairs[3] = {
+		{&c4,&p4},
+		{&c8,&p8},
+		{&c12,&p12}
+	};
+	
+	// calculate p4 over all x in z (series length 4),
+	// ibid. for p8 (series length 8)
+	// & finally p12 (series length 12)
+	for(auto pair: pairs) {
+		Matrix* coefficients = pair.first;
+		Vector* results = pair.second;
+		
+		// for each x value in the linspace,
+		// calculate p?(x), where ? iterates 4, 8, 12.
+		for(Index i = 0; i < z.size(); ++i) {
+			double x = z(i);
+			(*results)[i] = nest(*coefficients, x);
+		}
 	}
+	
+	// compute vector f by evaluating e^x at all x in z.
+	Vector f = z.row(0);
+	f = M_E ^ f;
+	std::cout << f << std::endl;
+	
+	
 	
     return 0;
 }
